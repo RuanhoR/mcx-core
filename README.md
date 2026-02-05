416d71d2b7e23c7f1a6cdbae1002025529d80be0
Ruanhor <3915264929@qq.com>
2026-02-05 20:07:22 +0800

    style: update config and docs

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # mcx
 
+mcx-core 是 mbler 的 mcx 功能的编译器核心，目前在 alpha 阶段。  
+你可以在 npm 上下载它，不过 npm 上的仅会在发布版本更新，github上为最新
+
+## 整体架构
+
+| **npm包名**             | **描述**                       | **完成度(%)** |
+|-----------------------|------------------------------|---------|
+| **@mbler/mcx**        |  为用户提供的typescript类型包         | 0       |
+| **@mbler/mcx-core**   | mcx核心编译器啥的代码                 | 10      |
+| **@mbler/mcx-compat** | mcx检查器规则，主要适配不一样的minecraft版本 | 0       |
+| **mbler**             | 核心脚手架，把这些东西组装成一个方便使用的cli工具   | 90      |
+
+## 加入开发
+
+前往作者QQ : 3915264929
+
+## 文档（没写完）
+
+**[这里](https://mbler-docs.ruanhor.dpdns.org/guide/mcx)**
--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,42 +1,37 @@
-import resolve from '@rollup/plugin-node-resolve';
-import json from '@rollup/plugin-json';
-import ts from "@rollup/plugin-typescript"
-import commonjs from '@rollup/plugin-commonjs';
-import dts from 'rollup-plugin-dts';
+import resolve from "@rollup/plugin-node-resolve";
+import json from "@rollup/plugin-json";
+import ts from "@rollup/plugin-typescript";
+import commonjs from "@rollup/plugin-commonjs";
+import dts from "rollup-plugin-dts";
 // 基础配置
 const main = {
-  input: 'src/index.ts', // 入口文件
+  input: "src/index.ts", // 入口文件
   output: [
     {
-      file: 'dist/index.cjs.js', // CommonJS
-      format: 'cjs',
-      sourcemap: false
-    }
+      file: "dist/index.cjs.js", // CommonJS
+      format: "cjs",
+      sourcemap: false,
+    },
   ],
-  plugins: [
-    resolve(),
-    json(),
-    commonjs(),
-    ts()
-  ],
+  plugins: [resolve(), json(), commonjs(), ts()],
   external: [
-    'node:fs/promises',
-    'node:path',
-    'node:os',
-    '@babel/types',
-    'acorn',
-    'acorn-walk',
-    'estree-walker',
-    'magic-string',
-    'source-map',
-    'typescript',
-    '@babel/parser'
-  ]
+    "node:fs/promises",
+    "node:path",
+    "node:os",
+    "@babel/types",
+    "typescript",
+    "@babel/parser",
+  ],
 };
-export default [main,
-{
-  input: 'dist/types/index.d.ts',
-  output: [{ file: 'dist/index.d.ts', format: 'es' }],
+const Dts = {
+  input: "src/index.ts",
+  output: {
+    file: "dist/index.d.ts",
+    format: "es",
+  },
   plugins: [dts()],
-}
+};
+export default [
+  main,
+  Dts
 ];

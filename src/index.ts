9ff5d9d0c5f294bf074b5a9e07b040dc63c1f6d2
Ruanhor <3915264929@qq.com>
2026-03-06 22:07:30 +0800

    refactor: remove default export index object from index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,20 +9,10 @@ import * as compile_component from "./mcx-component/index.js"
 import {
   transform
 } from "./transforms"
-const index = {
-  load: compiler,
-  AST: AST,
-  Compiler,
-  utils: utils,
-  compile_component,
-  plugin,
-  transform
-}
 
 export {
   PUBTYPE,
   compiler as compile,
-  index as default,
   AST,
   Compiler,
   utils,

739c45c358742a3ae4eae58a55a61a93219a7dce
ruanhor <3915264929@qq.com>
2026-08-27 21:43:44 +0800

    fix: update package versions to rc.1 for consistency

--- packages/client/src/lib/packCall.ts
+++ packages/client/src/lib/packCall.ts
@@ -2,11 +2,9 @@ import { World, world } from '@minecraft/server';
 type WorldProtoype = (typeof World)['prototype'];
 interface MockdWorld extends WorldProtoype {
   _mcx_packcall: {
-    subscribePack(): void;
-    listPack(): number[];
     createEvent(
       eventName: string,
-      handler: (packId: number, data: unknown) => unknown | Promise<unknown>,
+      handler: (data: unknown) => unknown | Promise<unknown>,
     ): void;
     push(
       eventName: string,
@@ -15,57 +13,47 @@ interface MockdWorld extends WorldProtoype {
     ): Promise<unknown> | unknown;
     on(
       eventName: string,
-      handler: (packId: number, data: unknown) => unknown | Promise<unknown>,
+      handler: Parameters<MockdWorld['_mcx_packcall']['createEvent']>[1],
     ): void;
+    eventMap: Map<
+      string,
+      Parameters<MockdWorld['_mcx_packcall']['createEvent']>[1]
+    >;
   };
 }
 let initd = false;
 export function init() {
   initd = true;
   const MockdWorld = World.prototype as MockdWorld;
-  if (MockdWorld._mcx_packcall) {
-    MockdWorld._mcx_packcall.subscribePack();
-  } else {
-    const packArr: number[] = [];
+  if (!MockdWorld._mcx_packcall) {
     let currentMaxPackId = 0;
-    const eventMap = new Map<
-      string,
-      Parameters<MockdWorld['_mcx_packcall']['createEvent']>[1]
-    >();
     MockdWorld._mcx_packcall = {
-      subscribePack() {
-        packArr.push(currentMaxPackId++);
-      },
-      listPack() {
-        return packArr;
-      },
+      eventMap: new Map(),
+
       createEvent(eventName, handler) {
-        if (eventMap.has(eventName)) {
+        if (this.eventMap.has(eventName)) {
           throw new TypeError(
             `[mcx PackCall]: Can't createEvent: event(${eventName}) is used`,
           );
         }
-        eventMap.set(eventName, handler);
+        this.eventMap.set(eventName, handler);
       },
-      push(eventName, data, packId) {
-        const subscribed = eventMap.get(eventName);
+      push(eventName, data) {
+        const subscribed = this.eventMap.get(eventName);
         if (!subscribed) {
           throw new TypeError(`[mcx PackCall]: Not find event ${eventName}`);
         }
-        if (!packArr.includes(packId)) {
-          throw new TypeError('[mcx internal]: invaild packId');
-        }
-        return subscribed(packId, data);
+        return subscribed(data);
       },
       on(eventName, handler) {
-        const old = eventMap.get(eventName);
-        if (!eventMap.has(eventName) || !old) {
+        const old = this.eventMap.get(eventName);
+        if (!this.eventMap.has(eventName) || !old) {
           throw new TypeError(
             `[mcx PackCall]: Can't exec on: event (${eventName}) is not subscribe`,
           );
         }
-        eventMap.set(eventName, (...args) => {
-          (handler(...args), old(...args));
+        this.eventMap.set(eventName, data => {
+          (handler(data), old(data));
         });
       },
     };
